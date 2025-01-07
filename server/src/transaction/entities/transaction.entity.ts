f0e1d88e677fa5a439f330cbc6cab7c94ca2887f
Denis Voinovich <denzelvojn@gmail.com>
2025-01-07 17:26:55 +0300

    Подключение PostgreSQL, TypeORM, связи таблиц

--- server/src/transaction/entities/transaction.entity.ts
+++ server/src/transaction/entities/transaction.entity.ts
@@ -1,1 +1,25 @@
-export class Transaction {}
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn, UpdateDateColumn } from 'typeorm';
+import { Category } from '../../category/entities/category.entity';
+import { User } from '../../user/entities/user.entity';
+
+@Entity()
+export class Transaction {
+	@PrimaryColumn({ name: 'transaction_id' })
+	id: number;
+	@Column()
+	title: string;
+	@Column({ nullable: true })
+	type: string;
+	@Column()
+	amount: number;
+	@CreateDateColumn()
+	createAt: Date;
+	@UpdateDateColumn()
+	updateAt: Date;
+	@ManyToOne(() => User, (user) => user.transactions)
+	@JoinColumn({ name: 'user_id' })
+	user: User;
+	@ManyToOne(() => Category, (category) => category.transactions)
+	@JoinColumn({ name: 'category_id' })
+	category: Category;
+}
--- server/src/user/entities/user.entity.ts
+++ server/src/user/entities/user.entity.ts
@@ -1,4 +1,25 @@
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { Category } from '../../category/entities/category.entity';
+import { Transaction } from '../../transaction/entities/transaction.entity';
+
+@Entity()
 export class User {
-	email: string;
+	@PrimaryGeneratedColumn()
+	id: number;
+	@Column()
+	name: string;
+	@Column()
 	password: string;
+	@CreateDateColumn()
+	createAt: Date;
+	@UpdateDateColumn()
+	updateAt: Date;
+	@OneToMany(() => Category, (category) => category.user, {
+		onDelete: 'CASCADE'
+	})
+	categories: Category[];
+	@OneToMany(() => Transaction, (transaction) => transaction.user, {
+		onDelete: 'CASCADE'
+	})
+	transactions: Transaction[];
 }
